d28a87509a48801680783060c2ec0e289df3ebad
Tim Holum <tholum@coulee.tech>
2024-12-16 19:58:24 -0600

    full working

--- nodes/MicrosoftSharepoint/helpers/misc.ts
+++ nodes/MicrosoftSharepoint/helpers/misc.ts
@@ -2,25 +2,74 @@ import { IExecuteFunctions, ILoadOptionsFunctions, NodeApiError } from "n8n-work
 import { makeMicrosoftRequest } from "./makeMicrosoftRequest";
 
 export async function MSGetSites(thisRef: IExecuteFunctions | ILoadOptionsFunctions): Promise<any> {
-	return await makeMicrosoftRequest(thisRef, 'sites', {
-		qs: {
-			search: '*',
+	let allSites: any[] = [];
+	let nextLink: string | undefined;
+
+	do {
+		const response = nextLink 
+			? await makeMicrosoftRequest(thisRef, nextLink)
+			: await makeMicrosoftRequest(thisRef, 'sites', {
+					qs: {
+						search: '*',
+						$top: 100,
+						$select: 'id,name,displayName,webUrl,siteCollection'
+					}
+				});
+
+		if (response.value) {
+			// Add more descriptive names including the site URL
+			const enhancedSites = response.value.map((site: any) => ({
+				...site,
+				displayName: `${site.displayName} (${site.webUrl})`,
+			}));
+			allSites = allSites.concat(enhancedSites);
 		}
-	});
+
+		nextLink = response['@odata.nextLink'];
+		if (nextLink) {
+			// Remove the base URL from nextLink as makeMicrosoftRequest will add it
+			nextLink = nextLink.replace('https://graph.microsoft.com/v1.0/', '');
+		}
+	} while (nextLink);
+
+	// Log the sites for debugging
+	thisRef.logger.info('Available SharePoint sites:', JSON.stringify(allSites, null, 2));
+
+	return { value: allSites };
 }
 
 export async function MSGetSiteDrives(thisRef: IExecuteFunctions | ILoadOptionsFunctions, siteId: string): Promise<any> {
-	return await makeMicrosoftRequest(thisRef, `sites/${siteId}/drives`);
+	let allDrives: any[] = [];
+	let nextLink: string | undefined;
+
+	do {
+		const response = nextLink
+			? await makeMicrosoftRequest(thisRef, nextLink)
+			: await makeMicrosoftRequest(thisRef, `sites/${siteId}/drives`, {
+					qs: {
+						$top: 100,
+						$select: 'id,name,driveType,webUrl'
+					}
+				});
+
+		if (response.value) {
+			// Add more descriptive names including the drive URL
+			const enhancedDrives = response.value.map((drive: any) => ({
+				...drive,
+				name: `${drive.name} (${drive.webUrl})`,
+			}));
+			allDrives = allDrives.concat(enhancedDrives);
+		}
+
+		nextLink = response['@odata.nextLink'];
+		if (nextLink) {
+			nextLink = nextLink.replace('https://graph.microsoft.com/v1.0/', '');
+		}
+	} while (nextLink);
+
+	// Log the drives for debugging
+	thisRef.logger.info('Available drives for site:', JSON.stringify(allDrives, null, 2));
+
+	return { value: allDrives };
 }
 
-export async function MSGetItemDetailsByPath(thisRef: IExecuteFunctions, libraryId: string, filePath: string): Promise<any> {
-    const folder = await makeMicrosoftRequest(thisRef, `drives/${libraryId}/root:/${filePath}:/`);
-    if(!folder || !folder.id){
-        // Do something
-        throw new NodeApiError(thisRef.getNode(), folder, {
-            message: "Could not find folder. Is your path correct?"
-        });
-    }
-
-    return folder;
-}
